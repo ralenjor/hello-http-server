62624182f0deebb68fbb2ff69ad1b5acfd472ee8
Chris <mr.christopher.hahn@gmail.com>
2026-03-09 18:59:07 -0400

    server functional

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// write your code here
+const http = require('http');
+
+const port = 8080;
+
+
+const server = http.createServer((req, res) => {
+
+    console.log(`${req.method} received for path: ${req.url}`);
+
+    res.writeHead(200, { 
+        'content-type': 'text/html',
+
+     });
+
+
+    res.end('Hello World');});
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+
+});
